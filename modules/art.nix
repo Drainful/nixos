6c6ad78fc9c335b65414b255c9cd4cfd160dc56d
Adrian Fullmer <ipkcle@gmail.com>
2019-04-12 02:25:27 -0400

    Use nixos unstable's krita derivation

--- modules/art.nix
+++ modules/art.nix
@@ -1,13 +1,18 @@
+let
+  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+in 
 { config, pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     gimp
-    krita
     inkscape
     mypaint
     mypaint-brushes
     libwacom
     xf86_input_wacom
+
+    # nixos stable's version of krita crashes when I use my pen
+    unstable.krita
   ];
 }
